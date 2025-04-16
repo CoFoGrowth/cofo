03f970d6926b4f378171487fdfa60dcbb20c6e7c
Andrii Stafiniak <andrii.stafiniak@gmail.com>
2025-04-16 13:21:16 +0200

    added new font

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -1,11 +1,19 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&display=swap');
+  
   body {
     margin: 0px;
     padding: 50px;
     background: #000000;
-    font-family: 'Arial', sans-serif;
+    font-family: "Outfit", sans-serif;
+  }
+  
+  .container {
+    max-width: 1600px;
+    margin: 0 auto;
+    width: 100%;
   }
 `;
 
