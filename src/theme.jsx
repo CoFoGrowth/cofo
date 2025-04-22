87ae0f7365a794452bf76169dfcc40a7e62b46f0
Andrii Stafiniak <andrii.stafiniak@gmail.com>
2025-04-22 13:56:12 +0200

    add new global colors

--- src/theme.jsx
+++ src/theme.jsx
@@ -1,8 +1,11 @@
 export const theme = {
   colors: {
     primary: "#06000e",
+    primaryDark: "#030007",
     secondary: "#2ecc71",
     accent: "#e74c3c",
+    background: "#0A011D",
+    text: "#333333",
   },
   media: {
     mobile: "768px",
